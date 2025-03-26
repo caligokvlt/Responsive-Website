060e815484c3ffe4e1305c0539c16e868f286819
caligokvlt <zoegreen36@gmail.com>
2025-03-25 23:05:22 -0600

    Commiiiitttting

--- JS/myscript.js
+++ JS/myscript.js
@@ -5,5 +5,19 @@ function openPage(pageName) {
         x[i].style.display = "none";
     }
     document.getElementById(pageName).style.display = "block";
-
+}
+lightMode = false;
+darkMode = true;
+function toggleLightDark() {
+    let currentMode = document.getElementById("current-mode");
+    if (darkMode == true) {
+        lightMode = true;
+        darkMode = false;
+        currentMode.href = "CSS/lightmode.css";
+    }
+    else {
+        lightMode = false;
+        darkMode = true;
+        currentMode.href = "CSS/darkmode.css";
+    }
 }
